cb6540cda921d3b2dbc3fe33b87e63e871153c2c
Juanan Ruiz <juanan@us.es>
2015-07-03 21:39:26 +0200

    Subida inicial del proyecto

---
+++ app/config/prod.php
@@ -0,0 +1,16 @@
+<?php
+
+// configure your app for the production environment
+
+date_default_timezone_set('Europe/Madrid');
+
+// DoctrineServiceProvider opciones para la BD
+$app['db.options'] = array(
+    'driver' => 'pdo_mysql',
+    'dbname' => 'encuestas',
+    'host' => 'localhost',
+    'user' => 'encuestador',
+    'password' => 'secretitosenreunionsonfaltadeeducacion',
+    'charset' => 'utf8',
+    'driverOptions' => array(1002 => 'SET NAMES utf8'),
+);
